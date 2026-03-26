5452125f5474fde1233c6c1ac32a7c9e47136f47
Rubenstein <mrubenstein@igsaaa071lem037.localdomain>
2026-03-26 11:53:58 -0400

    describe purpose of the EAU_prop_suitable script

---
+++ EAU_prop_suitable.R
@@ -0,0 +1,6 @@
+###ILP Formulation of the PPR Problem ###
+# March 2026
+
+### Establish Percent of Suitable Habitat for EAUs ####
+  # characterizing the percent of suitable habitat for each EAU
+  # is a critical first step to establishing both benefit, cost, and transition estimates
